b8c3d522086ae462aed9300737bdcf916ab0b826
Alexander Gonchar <aleksandrg@wix.com>
2018-06-22 11:17:15 +0300

    Proxy new TimePicker component from BackOffice. Add story for it.

--- .storybook/stories.js
+++ .storybook/stories.js
@@ -112,6 +112,7 @@ import '../stories/Tag.story.js'; // 12.5 Tag
 
 // 13. Work in progress
 import '../stories/Table'; // 13.1 Table Card
+import '../stories/TimePicker.story'; // 13.2 TimePicker (rewritten)
 
 // TPA
 import '../stories/TPA/Button';
